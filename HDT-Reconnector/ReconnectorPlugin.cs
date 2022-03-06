48f770e30e6bc0dc2648663b77a4a06b6d0f126a
Hao Ruan <righthandss@gmail.com>
2022-03-06 09:49:00 +0800

    Update plugin version

--- HDT-Reconnector/ReconnectorPlugin.cs
+++ HDT-Reconnector/ReconnectorPlugin.cs
@@ -25,7 +25,7 @@ namespace HDT_Reconnector
 
         public string Author => "Hypervisor";
 
-        public Version Version => Version.Parse("1.0.0");
+        public Version Version => Version.Parse("1.1.0");
 
         public MenuItem MenuItem { get; private set; }
 
