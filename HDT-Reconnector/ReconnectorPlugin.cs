ca2adfd75434b09d788b0e250fb953c5159351a9
Hao Ruan <righthandss@gmail.com>
2022-09-11 11:50:53 +0800

    Update reference to v1.18.20 to make simulator work

--- HDT-Reconnector/ReconnectorPlugin.cs
+++ HDT-Reconnector/ReconnectorPlugin.cs
@@ -25,7 +25,7 @@ namespace HDT_Reconnector
 
         public string Author => "Hypervisor";
 
-        public Version Version => Version.Parse("1.4.0");
+        public Version Version => Version.Parse("1.4.1");
 
         public MenuItem MenuItem { get; private set; }
 
